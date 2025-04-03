2dee165df0662d5b24742908354bcaa593ba7d5c
cgeils25 <cgeils@email.sc.edu>
2025-04-03 08:59:36 -0400

    add script to build dumbpy binaries

---
+++ build.sh
@@ -0,0 +1,19 @@
+echo "Building DumbPy binaries..."
+
+cmake .
+
+make 
+
+mkdir -p bin
+
+mv *.so bin
+
+export PYTHONPATH="${PYTHONPATH}:bin/"
+
+echo "
+DumbPy binary built and moved to bin directory. DumbPy can now be imported with 
+
+>>>'import dumbpy as dp'
+
+>>> dp.say_hello('Your Name')
+"
