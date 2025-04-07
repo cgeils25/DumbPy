3207b893983c809f71c110a720d2dfe5c95677ed
cgeils25 <cgeils@email.sc.edu>
2025-04-07 18:07:46 -0400

    import all_close

--- dumbpy/__init__.py
+++ dumbpy/__init__.py
@@ -1,6 +1,8 @@
 import _dumbpy_core
 __doc__ = _dumbpy_core.__doc__
 
+from _dumbpy_core import all_close
+
 from dumbpy import math, transform
 from dumbpy.say_hello import say_hello
 from dumbpy.matrix import Matrix
