eb0df3623659544d03f9ffab2d8905c528eb5aa4
cgeils25 <cgeils@email.sc.edu>
2025-04-07 17:42:16 -0400

    implement all matrix clas tests - passing

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -37,23 +37,23 @@ def test_setitem():
 
     new_val = 4
 
-    a[1][1] = new_val
+    a[1, 1] = new_val
 
-    assert a[1][1] == new_val, f"Expected {a[1][1]} to be 4"
+    assert a[1, 1] == new_val, f"Expected {a[1, 1]} to be 4"
 
-    assert a[1][1] != data[1][1], f"Expected {a[1][1]} to be not equal to {data[1][1]}"
+    assert a[1, 1] != data[1][1], f"Expected {a[1, 1]} to be not equal to {data[1][1]}"
 
     print("Set item test passed!")
 
 def test_equality():
-    data = [1, 2, 3]
+    data = [[1, 2, 3], [4, 5, 6]]
 
-    a = dp.Vector(data)
-    b = dp.Vector(data)
+    a = dp.Matrix(data)
+    b = dp.Matrix(data)
 
     assert a == b, f"Expected {a} to be equal to {b}"
 
-    a[0] = 2
+    a[0, 0] = 2
 
     assert a != b, f"Expected {a} to be not equal to {b}"
 
@@ -61,15 +61,16 @@ def test_equality():
 
 def test_repr():
     # tried to make this pretty broad. Just makes sure the value is in the representation and the output is a string
-    data = [1, 2, 3]
+    data = [[1, 2, 3], [4, 5, 6]]
 
-    a = dp.Vector(data)
+    a = dp.Matrix(data)
 
     repr_out = repr(a)
 
     assert isinstance(repr_out, str), f"Expected {repr(a)} to be a string"
 
-    for value in data:
-        assert str(value) in repr_out, f"Expected {value} to be in {repr_out}"
+    for row in data:
+        for value in row:
+            assert str(value) in repr_out, f"Expected {value} to be in {repr_out}"
 
     print("Repr test passed!")
