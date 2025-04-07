f16454452d55227d5dd3daeb1057c0deecad5371
cgeils25 <cgeils@email.sc.edu>
2025-04-07 15:15:16 -0400

    add tests for vectors

--- tests/test_matrix_math.py
+++ tests/test_matrix_math.py
@@ -1,9 +1,4 @@
 import dumbpy as dp
-data = [[1, 2], [3, 4]]
-m1 = dp.Matrix(2, 2)
-m1.set_values(data)
-print(m1)
 
-r = dp.math.add(m1, m1)
-
-print(r)
+if __name__ == "__main__":
+    print("-"*50, "Testing vector math...", "-"*50)
--- tests/test_vector_math.py
+++ tests/test_vector_math.py
@@ -1,21 +1,72 @@
 import dumbpy as dp
 
-v = dp.Vector(3)
+def test_instantiation():
+    data = [1, 2, 3]
 
-v.set_values([1, 2, 3])
+    a = dp.Vector(data)
 
-r = dp.math.add(v, v)
+    isinstance(a, dp.Vector), f"Expected {a} to be an instance of Vector"
 
-r.print()
+    print("Instantiation test passed!")
 
-r = dp.math.subtract(v, 2)
+def test_setitem():
+    data = [1, 2, 3]
+    a = dp.Vector(data)
 
-r.print()
+    assert a[1] == data[1], f"Expected {a[1]} to be {data[1]}"
 
-print('Attempting to get item ')
+    new_val = 4
 
-print(r[0])
+    a[1] = new_val
 
-r[0] = 10
+    assert a[1] == new_val, f"Expected {a[1]} to be 4"
 
-print(r[0])
+    assert a[1] != data[1], f"Expected {a[1]} to be not equal to {data[1]}"
+
+    print("Set item test passed!")
+
+def test_equality():
+    data = [1, 2, 3]
+
+    a = dp.Vector(data)
+    b = dp.Vector(data)
+
+    assert a == b, f"Expected {a} to be equal to {b}"
+
+    a[0] = 2
+
+    assert a != b, f"Expected {a} to be not equal to {b}"
+
+    print("Equality test passed!")
+
+def test_vector_add():
+    a = dp.Vector([1, 2, 3])
+    b = dp.Vector([4, 5, 6])
+    c = dp.math.add(a, b)
+    assert c == dp.Vector([5, 7, 9]), f"Expected {c} to be [5, 7, 9]"
+
+    d = dp.math.add(a, 2)
+
+    assert d == dp.Vector([3, 4, 5]), f"Expected {d} to be [3, 4, 5]"
+
+    print("Addition test passed!")
+
+def test_vector_sub():
+    a = dp.Vector([1, 2, 3])
+    b = dp.Vector([4, 5, 6])
+    c = dp.math.subtract(a, b)
+    assert c == dp.Vector([-3, -3, -3]), f"Expected {c} to be [-3, -3, -3]"
+
+    d = dp.math.subtract(a, 2)
+    assert d == dp.Vector([-1, 0, 1]), f"Expected {d} to be [-1, 0, 1]"
+
+    print("Subtraction test passed!")
+
+if __name__ == "__main__":
+    print("-"*50, "Testing matrix math...", "-"*50)
+
+    test_instantiation()
+    test_setitem()
+    test_equality()
+    test_vector_add()
+    test_vector_sub()
