06958164af31d161dc7451a9f9c8daad3cf6ba60
cgeils25 <cgeils@email.sc.edu>
2025-04-14 10:02:29 -0400

    check that results match for raw python and dumbpy matrix multiplication implementations

--- benchmark.py
+++ benchmark.py
@@ -2,6 +2,7 @@ import dumbpy as dp
 from timeit import timeit
 import random
 
+
 def matmul(a, b):
     result = []
 
@@ -13,6 +14,7 @@ def matmul(a, b):
     
     return result
 
+
 def benchmark_matmul(a: list, b: list, iterations: int = 1000):
     py_time = timeit(lambda: matmul(a, b), number=iterations)
 
@@ -23,6 +25,22 @@ def benchmark_matmul(a: list, b: list, iterations: int = 1000):
 
     return py_time, dp_time
 
+
+def test_result(a, b):
+    try:
+        out_py = matmul(a, b)
+
+        a_dp = dp.Matrix(a)
+        b_dp = dp.Matrix(b)
+        out_dp = dp.math.mat_mul(a_dp, b_dp)
+
+        assert dp.all_close(dp.Matrix(out_py), out_dp, 1e-4), "Results do not match!"
+    
+    except Exception as e:
+        print(f"Test failed with exception: {e}")
+        raise
+
+
 def main():
     n_iterations = 100
     n_rows = 300
@@ -30,7 +48,13 @@ def main():
 
     a = [[random.random() for _ in range(n_cols)] for _ in range(n_rows)]
     b = [[random.random() for _ in range(n_rows)] for _ in range(n_cols)]
+    
+    print('-' * 100)
+    print("Testing that matrix multiplication results match for python and DumbPy:")
+    test_result(a, b)
+    print("Test passed!")
 
+    print('-' * 100)
     print("Matrix Multiplication Benchmark:")
     py_time, dp_time = benchmark_matmul(a, b, iterations=n_iterations)
     print(f"Python time: {round(py_time, 3)} seconds")
